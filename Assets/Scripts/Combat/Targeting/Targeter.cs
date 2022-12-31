4a91d584d26ed6d57060c217074cfa08bd59d677
Melike Soydan <63014774+aiakosnyks@users.noreply.github.com>
2023-01-01 02:12:34 +0300

    Targeter fixed

--- Assets/Scripts/Combat/Targeting/Targeter.cs
+++ Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -7,15 +7,15 @@ public class Targeter : MonoBehaviour
     public List<Target> targets = new List<Target>();
     private void OnTriggerEnter(Collider other)
     {
-        Target target = other.GetComponent<Target>();
-        if(target == null) {return;}
+        if(!other.TryGetComponent<Target>(out Target target)) {return;}
         targets.Add(target);
     }
 
      private void OnTriggerExit(Collider other)
     {
-        Target target = other.GetComponent<Target>();
-        if(target == null) {return;}
+        if(!other.TryGetComponent<Target>(out Target target)) {return;}
         targets.Remove(target);
+        //Target target = other.GetComponent<Target>();
+        //if(target == null) {return;}
     }
 }
