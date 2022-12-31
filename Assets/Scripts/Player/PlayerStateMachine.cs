eac284ede235bdc91d048e23848ea343bacb4e07
Melike Soydan <63014774+aiakosnyks@users.noreply.github.com>
2022-12-31 23:24:59 +0300

    about to create an enviroment

--- Assets/Scripts/Player/PlayerStateMachine.cs
+++ Assets/Scripts/Player/PlayerStateMachine.cs
@@ -9,6 +9,8 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] public Animator Animator {get; private set;}
 
     [field: SerializeField] public float FreeLookMovementSpeed {get; private set;}
+    [field: SerializeField] public float RotationDamping {get; private set;}
+
     public Transform MainCameraTransform {get; private set;}
 
     void Start()
