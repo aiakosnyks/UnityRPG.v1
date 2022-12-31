bbc5fa382252e0c57d403c26c3a8da1d26a977b9
Melike Soydan <63014774+aiakosnyks@users.noreply.github.com>
2023-01-01 01:57:04 +0300

    Targeter added.

---
+++ Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targeter : MonoBehaviour
+{
+    public List<Target> targets = new List<Target>();
+    private void OnTriggerEnter(Collider other)
+    {
+        Target target = other.GetComponent<Target>();
+        if(target == null) {return;}
+        targets.Add(target);
+    }
+
+     private void OnTriggerExit(Collider other)
+    {
+        Target target = other.GetComponent<Target>();
+        if(target == null) {return;}
+        targets.Remove(target);
+    }
+}
