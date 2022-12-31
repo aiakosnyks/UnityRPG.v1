320d108d1b3d28f9c2cf2eeff781b974c71c35c2
Melike Soydan <63014774+aiakosnyks@users.noreply.github.com>
2022-12-31 17:46:14 +0300

    Commit

--- Assets/Scripts/Player/PlayerStateMachine.cs
+++ Assets/Scripts/Player/PlayerStateMachine.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine
 {
-    // Start is called before the first frame update
+    public InputReader InputReader {get; private set;};
     void Start()
     {
         SwithState(new PlayerTestState(this));
